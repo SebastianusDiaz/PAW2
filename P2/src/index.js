8f4d27f4e6572bff1a6d71f0d93650ec0b4e15e6
SebastianusDiaz <sebastianusdiaz@gmail.com>
2024-10-21 21:33:40 +0700

    Add files via upload

---
+++ P2/src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/about', (req, res) => {
+  res.send('Halaman About');
+});
+
+app.get('/contact', (req, res) => {
+  res.send('Halaman Contact Perusahaan');
+});
+
+//middleware
+app.use('*', (req, res) => {
+  res.status(404);
+  res.send("404, Halaman tidak ditemukan");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
